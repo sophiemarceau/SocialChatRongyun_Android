f5c98f7038162a857e5deb0b35636f7832082b00
sophiemarceau <sophiemarceauqu@gmail.com>
2019-11-07 03:32:35 +0800

    4  add timeUtils

--- app/src/main/java/com/sophiemarceau_qu/meetchat/MainActivity.java
+++ app/src/main/java/com/sophiemarceau_qu/meetchat/MainActivity.java
@@ -5,10 +5,19 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.sophiemarceau_qu.framework.utils.LogUtils;
+import com.sophiemarceau_qu.framework.utils.TimeUtils;
+
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        LogUtils.i("hellword");
+        LogUtils.e("sophiemarceau");
+
+        LogUtils.i(TimeUtils.formatDuring(System.currentTimeMillis()));
     }
 }
--- framework/src/main/java/com/sophiemarceau_qu/framework/utils/LogUtils.java
+++ framework/src/main/java/com/sophiemarceau_qu/framework/utils/LogUtils.java
@@ -1,5 +1,6 @@
 package com.sophiemarceau_qu.framework.utils;
 
+import android.os.Environment;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -26,7 +27,7 @@ public class LogUtils {
             //每次调用都会判断是否为空 release
             if (!TextUtils.isEmpty(text)) {
                 Log.i(BuildConfig.LOG_TAG, text);
-                writeToFile(text);
+//                writeToFile(text);
             }
         }
     }
@@ -36,29 +37,29 @@ public class LogUtils {
             //每次调用都会判断是否为空 release
             if (!TextUtils.isEmpty(text)) {
                 Log.e(BuildConfig.LOG_TAG, text);
-                writeToFile(text);
+//                writeToFile(text);
             }
         }
     }
 
-    public static void writeToFile(String text) {
+    private static void writeToFile(String text) {
         //文件路径
-        String fileName = "/sdcard/SocialChat/SocialChat.log";
-        //时间 等级 内存
-        String log = mSimpleDateFormat.format(new Date() + "" + text);
+        String fileName = "/sdcard/meetchat/meetchat.log";
+        // 时间 + 内容
+        String log = mSimpleDateFormat.format(new Date()) + " " + text + "\n";
         //检查父路径
-        File fileGroup = new File("/sdcard/SocialChat/");
+        File fileGroup = new File("/sdcard/meetchat/");
         if (!fileGroup.exists()) {
             fileGroup.mkdirs();
         }
-
         //开始写入
         FileOutputStream fileOutputStream = null;
         BufferedWriter bufferedWriter = null;
         try {
             fileOutputStream = new FileOutputStream(fileName, true);
-            //编码问题 GBK 中文的存入
-            bufferedWriter = new BufferedWriter(new OutputStreamWriter(fileOutputStream, Charset.forName("gbk")));
+            //编码问题 GBK 正确的存入中文
+            bufferedWriter = new BufferedWriter(
+                    new OutputStreamWriter(fileOutputStream, Charset.forName("gbk")));
             bufferedWriter.write(log);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -72,16 +73,7 @@ public class LogUtils {
                     e.printStackTrace();
                 }
             }
-
-            if (fileOutputStream != null) {
-                try {
-                    fileOutputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
-
     }
 
 
